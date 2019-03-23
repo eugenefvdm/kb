f0fa3e38937599a1bc8ff90278a41450dd893dc6
Eugene van der Merwe <eugenevdm@gmail.com>
2019-03-23 15:13:01 +0200

    trying more wrap tricks

--- resources/views/vendor/nova/layout.blade.php
+++ resources/views/vendor/nova/layout.blade.php
@@ -29,8 +29,15 @@
             padding: 1em 1.5em;
             display: block;
             word-wrap: break-word;
-            
+
             white-space:pre-wrap;
+
+            white-space: pre-wrap;       /* css-3 */
+            white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+            /*white-space: -pre-wrap;      !* Opera 4-6 *!*/
+            white-space: -o-pre-wrap;    /* Opera 7 */
+            word-wrap: break-word;       /* Internet Explorer 5.5+ */
+
         }
     </style>
 
